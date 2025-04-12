5697a2f66622f953f2ea70febe06add55904f8fe
FBetulDemir <fatmabetuld@gmail.com>
2025-04-12 08:56:13 +0200

    Fixed folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,19 @@ function App() {
   
 
   return (
-    <>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to ISushi</h1>
+        
+      </header>
+      <main>
+        
 
-    </>
+      </main>
+      <footer>
+        <p>&copy; 2025 ISushi. Alla rättigheter förbehållna. Integritetspolicy | Användarvillkor</p>
+      </footer>
+    </div>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of ISushi.</p>
+    </div>
+  );
+}
+
+export default Home;
