b229e0b6de7565fcfc5b18c10f23a8b6c7322ecb
albingardelov <albin@gardelov.com>
2025-04-15 12:00:45 +0200

    Lagt till lite styling

--- src/components/receipt.jsx
+++ src/components/receipt.jsx
@@ -7,18 +7,22 @@ const Receipt = () => {
 
     return (
 		<>
-		<img src={receiptImg} alt="receipt" />
-		<section className="receipt"> 
-			 <h2> Tack för din beställning </h2>
-			 <p className="order-number"> #1235566 </p>
-			 Maki roll x2 45kr
-			 sushi mega x2 50kr
-			 sushi oma x2 68kr
+		<div className="receipt-wrapper">
+			<img src={receiptImg} alt="receipt" className="receipt-bg" />
+			<section className="receipt-container"> 
+				<h2> Tack för din beställning </h2>
+				<p className="order-number"> #1235566 </p>
+				{/* Här ska det komma script, som hämtar data från kundvagnen */}
+				Maki roll x2 45kr
+				sushi mega x2 50kr
+				sushi oma x2 68kr
 
-			 totalt 326kr
-			 <button className="new-order"> Ny beställning </button>
+				totalt 326kr
+				<button className="new-order"> Ny beställning </button>
 
-		</section>
+			</section>
+
+		</div>
 		</>
     );
 };
