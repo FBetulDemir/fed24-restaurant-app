28c192ea02d4f135b39291e3f4bb0c5bcb8a8b77
albingardelov <albin@gardelov.com>
2025-04-15 12:15:21 +0200

    fått till min layout, behöver nu skriva skript för att hämta data från kundvagnen

--- src/components/receipt.jsx
+++ src/components/receipt.jsx
@@ -3,28 +3,24 @@ import receiptImg from "../assets/receipt.png";
 import "../styles/receipt.css";
 
 const Receipt = () => {
-
-
-    return (
-		<>
-		<div className="receipt-wrapper">
-			<img src={receiptImg} alt="receipt" className="receipt-bg" />
-			<section className="receipt-container"> 
-				<h2> Tack för din beställning </h2>
-				<p className="order-number"> #1235566 </p>
-				{/* Här ska det komma script, som hämtar data från kundvagnen */}
-				Maki roll x2 45kr
-				sushi mega x2 50kr
-				sushi oma x2 68kr
-
-				totalt 326kr
-				<button className="new-order"> Ny beställning </button>
-
-			</section>
-
-		</div>
-		</>
-    );
+  return (
+    <div className="receipt-layout">
+      <div className="receipt-wrapper">
+        <img src={receiptImg} alt="receipt" className="receipt-bg" />
+        <section className="receipt-container"> 
+          <h2>Tack för din beställning</h2>
+          <p className="order-number">#1235566</p>
+          Maki roll x2 45kr<br />
+          Sushi mega x2 50kr<br />
+          Sushi oma x2 68kr<br />
+          <br />
+          Totalt 326kr
+          <br /><br />
+          <button className="new-order">Ny beställning</button>
+        </section>
+      </div>
+    </div>
+  );
 };
 
-export default Receipt
+export default Receipt;
