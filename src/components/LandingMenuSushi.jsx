a4834784a6e3e4f0d8f89e9e1a87e8c39c591897
FBetulDemir <fatmabetuld@gmail.com>
2025-04-16 13:01:16 +0200

    Added sushi section style to landing page

--- src/components/LandingMenuSushi.jsx
+++ src/components/LandingMenuSushi.jsx
@@ -5,15 +5,21 @@ const LandingMenuSushi = () => {
   return (
     <div className="landing-menu-sushi">
         <img src={sushiMenu} alt="Different kind of sushi on a plate with soja souce and chopsticks on the side." />
-        <h1>SUSHI MENU</h1>
-        <p>Upptäck vår noggrant utvalda sushimeny med klassiska favoriter och moderna smaker. Från delikat nigiri till kreativa makirullar – 
-            varje bit är tillagad med färska råvaror och hantverksskicklighet.
-        </p>
-        <div className="sushi-preview">
-            <ul className="sushi">
-                <li></li>
-            </ul>
+        <div className="sushi-info">
+            <h1>SUSHI MENU</h1>
+            <p>Upptäck vår noggrant utvalda sushimeny med klassiska favoriter och moderna smaker. Från delikat nigiri till kreativa makirullar – 
+                varje bit är tillagad med färska råvaror och hantverksskicklighet.
+            </p>
+            <div className="sushi-preview">
+                <ul>
+                    <li>Lax Nigiri</li>
+                    <li>Spicy Tuna Maki</li>
+                    <li>Avokado Maki</li>
+                </ul>
+            </div>
+            <button className="sushi-button">UPPTÄCK HELA MENYN</button>
         </div>
+
     </div>
   );
 }
