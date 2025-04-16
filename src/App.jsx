c4d76067ebe6da367e086f085ccc4455b0312141
Timmy Bartelius <timmy.bartelius1@gmail.com>
2025-04-16 18:48:07 +0200

    Flyttade på Cart till pages då det bråkade i min console

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import './App.css'
-import Cart from './pages/Cart.jsx'
-
+import "./App.css";
+import Cart from "./pages/components/Cart";
 
 function App() {
-  
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to ISushi</h1>
-        
       </header>
       <main>
         <Cart />
-
       </main>
       <footer>
-        <p>&copy; 2025 ISushi. Alla rättigheter förbehållna. Integritetspolicy | Användarvillkor</p>
+        <p>
+          &copy; 2025 ISushi. Alla rättigheter förbehållna. Integritetspolicy |
+          Användarvillkor
+        </p>
       </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
