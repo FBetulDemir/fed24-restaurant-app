b677671a4a5f5d45de3718de8e3f6692ff116cad
Timmy Bartelius <timmy.bartelius1@gmail.com>
2025-04-16 18:43:08 +0200

    Började med functions

--- components/Cart-functions.jsx
+++ components/Cart-functions.jsx
@@ -1,6 +1,7 @@
 //3. Det skall finnas en bild på rätten, en snabb beskrivning av vad som finns i den (alltså ingredienser) och även en lägg till/ta bort funktion med en counter därtill.
 
-import React, { useState } from "react";
+const adding = document.getElementById("addBtn");
+adding.addEventListener("onclick", addWare + 1);
 
 function addWare() {}
 
