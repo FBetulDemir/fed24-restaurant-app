7651b0907f62e9d45efc10dabcf77741c93b056e
FBetulDemir <fatmabetuld@gmail.com>
2025-04-16 10:25:48 +0200

    Updated menu page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -5,18 +5,10 @@ const Menu = () => {
     const { menuId } = useParams()
     return (
         <div className="menu">
-            <h1>Vår meny </h1>
-            <h2>Sushi</h2>
             <MakiSushi />
 
-            <h2>Sashimi</h2>
-                <ul>
-                    <li>Lax Sashimi</li>
-                    <li>Tonfisk Sashimi</li>
-                </ul>
             { menuId  ? <p>Här kan du se vår sushi och sashimi.</p> : <p>Page not found</p>}
             
-
         </div>
     )
   }
