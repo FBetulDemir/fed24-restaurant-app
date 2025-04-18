043baf308e38848d8a47b3790b35b14e8758fcb4
FBetulDemir <fatmabetuld@gmail.com>
2025-04-18 10:33:09 +0200

    Fixed AdminDishForm component to add new dishes to the menu: improved form validation, and added AdminStart component for sidebar navigation.

--- src/stores/menuStore.js
+++ src/stores/menuStore.js
@@ -3,7 +3,10 @@ import { create } from 'zustand'
 export const useMenuStore = create((set) => ({
     menuItems: [],
     setMenuItems: (items) => set({ menuItems: items }),
-    addItem: (item) => set((state) => ({ menuItems: [...state.menuItems, item] })),
+    addItem: (item) =>
+      set((state) => ({
+        menuItems: [...state.menuItems, item]
+      })),
     removeItem: (id) => set((state) => ({
       menuItems: state.menuItems.filter((item) => item.id !== id)
     })),
