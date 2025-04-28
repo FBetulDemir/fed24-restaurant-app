341d31a2a9016711bd47c985bae708f79d550a75
Wansiri-D <112197738+Wansiri-D@users.noreply.github.com>
2025-04-28 22:16:13 +0200

    deleted navlink EditMenu

--- src/components/AdminStart.jsx
+++ src/components/AdminStart.jsx
@@ -5,36 +5,25 @@ import MenuResetButton from "./MenuResetButton";
 import { useNavigate } from "react-router-dom";
 import useAdminStore from '../stores/authorizationStore.js';
 
-
-
 const AdminStart = () => {
-
-const navigate = useNavigate();
-const logout = useAdminStore((state) => state.logout);
-
-
-
+  const navigate = useNavigate();
+  const logout = useAdminStore((state) => state.logout);
 
   return (
     <div className="sidebar">
-        <h2>Välkommen tillbaka!</h2>
-        <NavLink to="/admin/add">
-            <button className="form-btn">Lägg till ny maträtt</button>
-        </NavLink>
-        <NavLink to="/pages/editMenu">
-            <button className="form-btn">Redigera/Ta bort meny</button>
-        </NavLink>
-        <NavLink to="/pages/editmenu">Edit Menu</NavLink>
-        
-        {/* <UploadAllMenusButton />    Jag testar ändringar
-        <MenuResetButton /> */}
-        <UploadAllMenusButton />   
-        <MenuResetButton />
-
-        <NavLink to="/">
-            <button className="form-btn">Logga ut</button>
-        </NavLink>
-  </div>
+      <h2>Välkommen tillbaka!</h2>
+      <NavLink to="/admin/add">
+        <button className="form-btn">Lägg till ny maträtt</button>
+      </NavLink>
+      <NavLink to="/pages/editMenu">
+        <button className="form-btn">Redigera/Ta bort meny</button>
+      </NavLink>
+      <UploadAllMenusButton />
+      <MenuResetButton />
+      <NavLink to="/">
+        <button className="form-btn">Logga ut</button>
+      </NavLink>
+    </div>
   );
 }
 
