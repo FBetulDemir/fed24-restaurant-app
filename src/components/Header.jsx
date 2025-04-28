6f5da06de7f5854501301c48d75dce166309cb1f
Timmy Bartelius <timmy.bartelius1@gmail.com>
2025-04-28 10:43:52 +0200

    Responsiv design - knappar, footer, header etc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,40 @@
 import { NavLink } from "react-router";
 import { useState, useRef } from "react";
 import "../styles/Header.css";
-import { useCartStore } from '../stores/cartStore'
+import { useCartStore } from "../stores/cartStore";
 import useOutsideClick from "./useOutsideClick";
 
-
-
-
-
-
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [hamburgerOpen, setHamburgerOpen] = useState(false);
-    const cart = useCartStore((state) => state.cart)
-    const cartCount = cart.length
-    const dropdownRef = useRef(null);
-    
+  const [isOpen, setIsOpen] = useState(false);
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+  const cart = useCartStore((state) => state.cart);
+  const cartCount = cart.length;
+  const dropdownRef = useRef(null);
 
+  const toggleDropdown = () => {
+    setIsOpen((prev) => {
+      if (!prev) setHamburgerOpen(false);
+      return !prev;
+    });
+  };
 
+  const toggleHamburger = () => {
+    setHamburgerOpen((prev) => {
+      if (!prev) setIsOpen(false);
+      return !prev;
+    });
+  };
 
-    const toggleDropdown = () => {
-        setIsOpen((prev) => {
-          if (!prev) setHamburgerOpen(false)
-          return !prev
-        })
-    }
-      
-    const toggleHamburger = () => {
-        setHamburgerOpen((prev) => {
-          if (!prev) setIsOpen(false)
-          return !prev
-        })
-    }
+  useOutsideClick(dropdownRef, () => {
+    setIsOpen(false);
+  });
 
-    useOutsideClick(dropdownRef, () => {
-        setIsOpen(false);
-      });
-      
-
-    return (
-        <header className="header">
-            <nav className="nav">
-                <ul className="nav-left">
-                    {/* <li><NavLink to='/pages/about' className="navlink">OM</NavLink></li> */}
-                    {/* <li className="dropdown">
+  return (
+    <header className="header">
+      <nav className="nav">
+        <ul className="nav-left">
+          {/* <li><NavLink to='/pages/about' className="navlink">OM</NavLink></li> */}
+          {/* <li className="dropdown">
                         <button onClick={toggleDropdown} className="dropbtn btn-dark">
                             MENY 
                             <svg
@@ -73,56 +64,97 @@ const Header = () => {
                             </div>
                         )}
                     </li> */}
-                    <li>
-                        <button className="dropbtn btn-dark">
-                            <NavLink to="/pages/menu/menu/" className="navlink-footer">Meny
-                            </NavLink>
-                        </button>
-                    </li>
-                </ul>
-                <div className="logo-container">
-                   <NavLink to='/' className="logo">ISUSHI</NavLink>
-                </div>
+          <li>
+            <button className="dropbtn btn-dark">
+              <NavLink to="/pages/menu/menu/" className="navlink-footer">
+                Meny
+              </NavLink>
+            </button>
+          </li>
+        </ul>
+        <div className="logo-container">
+          <NavLink to="/" className="logo">
+            ISUSHI
+          </NavLink>
+        </div>
 
-                <button className="hamburger-button" onClick={toggleHamburger}>
-                    <span className="hamburger-line"></span>
-                    <span className="hamburger-line"></span>
-                    <span className="hamburger-line"></span>
-                </button>
+        <button className="hamburger-button" onClick={toggleHamburger}>
+          <span className="hamburger-line"></span>
+          <span className="hamburger-line"></span>
+          <span className="hamburger-line"></span>
+        </button>
 
-                <div className="dropdown-content" style={{ display: hamburgerOpen ? 'block' : 'none' }}>
-                    
-                    <NavLink to="/" className="navlink" onClick={() => setHamburgerOpen(false)}></NavLink>
-                    {/* <NavLink to="/pages/about" className="navlink" onClick={() => setHamburgerOpen(false)}>OM</NavLink> */}
-                    <NavLink to="/pages/login" className="navlink" onClick={() => setHamburgerOpen(false)}>
-                        {/* <button className="btn-dark">LOGGA IN</button> */}
-                    </NavLink>
-                    <NavLink to="/pages/orderOnline" className="navlink" onClick={() => setHamburgerOpen(false)}>
-                        <button className="btn-dark">BESTÄLL ONLINE</button>
-                    </NavLink>
-                    
-                </div>
-                            
-                <ul className="nav-right">
-                    <li className="cart-icon-wrapper">
-                        <NavLink to="components/cart/:cartId?" className="navlink">
-                            <button className="btn-dark cart-button">
-                                {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <g opacity="0.3">
-                                        <path d="M9 22C9.55228 22 10 21.5523 10 21C10 20.4477 9.55228 20 9 20C8.44772 20 8 20.4477 8 21C8 21.5523 8.44772 22 9 22Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                        <path d="M20 22C20.5523 22 21 21.5523 21 21C21 20.4477 20.5523 20 20 20C19.4477 20 19 20.4477 19 21C19 21.5523 19.4477 22 20 22Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                        <path d="M1 1H5L7.68 14.39C7.77144 14.8504 8.02191 15.264 8.38755 15.5583C8.75318 15.8526 9.2107 16.009 9.68 16H19.4C19.8693 16.009 20.3268 15.8526 20.6925 15.5583C21.0581 15.264 21.3086 14.8504 21.4 14.39L23 6H6" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                    </g>
-                                </svg>
-                            </button>
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>    
+        <div
+          className="dropdown-content"
+          style={{ display: hamburgerOpen ? "block" : "none" }}
+        >
+          <NavLink
+            to="/"
+            className="navlink"
+            onClick={() => setHamburgerOpen(false)}
+          ></NavLink>
+          {/* <NavLink to="/pages/about" className="navlink" onClick={() => setHamburgerOpen(false)}>OM</NavLink> */}
+          <NavLink
+            to="/pages/login"
+            className="navlink"
+            onClick={() => setHamburgerOpen(false)}
+          >
+            {/* <button className="btn-dark">LOGGA IN</button> */}
+          </NavLink>
+          <NavLink
+            to="/pages/orderOnline"
+            className="navlink"
+            onClick={() => setHamburgerOpen(false)}
+          >
+            <button className="btn-dark">BESTÄLL ONLINE</button>
+          </NavLink>
+        </div>
 
-    )
-}
+        <ul className="nav-right">
+          <li className="cart-icon-wrapper">
+            <NavLink to="components/cart/:cartId?" className="navlink">
+              <button className="btn-dark cart-button">
+                {cartCount > 0 && (
+                  <span className="cart-count">{cartCount}</span>
+                )}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                >
+                  <g opacity="0.3">
+                    <path
+                      d="M9 22C9.55228 22 10 21.5523 10 21C10 20.4477 9.55228 20 9 20C8.44772 20 8 20.4477 8 21C8 21.5523 8.44772 22 9 22Z"
+                      stroke="white"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M20 22C20.5523 22 21 21.5523 21 21C21 20.4477 20.5523 20 20 20C19.4477 20 19 20.4477 19 21C19 21.5523 19.4477 22 20 22Z"
+                      stroke="white"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M1 1H5L7.68 14.39C7.77144 14.8504 8.02191 15.264 8.38755 15.5583C8.75318 15.8526 9.2107 16.009 9.68 16H19.4C19.8693 16.009 20.3268 15.8526 20.6925 15.5583C21.0581 15.264 21.3086 14.8504 21.4 14.39L23 6H6"
+                      stroke="white"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </g>
+                </svg>
+              </button>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -1,45 +1,42 @@
-import heroVideo from '../assets/sushi-on-fire.mp4';
+import heroVideo from "../assets/sushi-on-fire.mp4";
 import "../styles/MainBanner.css";
 import { NavLink } from "react-router-dom";
 import { useRef, useState } from "react";
 
 const MainBanner = () => {
-    const videoRef = useRef(null);
-    const [isPaused, setIsPaused] = useState(false);
+  const videoRef = useRef(null);
+  const [isPaused, setIsPaused] = useState(false);
 
-    const toggleVideo = () => {
-        if (videoRef.current) {
-            if (videoRef.current.paused) {
-                videoRef.current.play();
-                setIsPaused(false);
-            } else {
-                videoRef.current.pause();
-                setIsPaused(true);
-            }
-        }
-    };
+  const toggleVideo = () => {
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+        setIsPaused(false);
+      } else {
+        videoRef.current.pause();
+        setIsPaused(true);
+      }
+    }
+  };
 
-    return (
-        <div className="main-banner">
-            <video
-                ref={videoRef}
-                autoPlay
-                loop
-                muted
-                src={heroVideo}
-                className="video-background"
-                type="video/mp4"
-            />
-            <div className="video-overlay">
-                <NavLink to="/pages/orderOnline" className="navlink">
-                    <button className="btn-hero">BESTÄLL ONLINE</button>
-                </NavLink>
-                <button className="btn-pause btn-hero" onClick={toggleVideo}>
-                    {isPaused ? "SPELA UPP" : "PAUSA VIDEO"}
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="main-banner">
+      <video
+        ref={videoRef}
+        autoPlay
+        loop
+        muted
+        src={heroVideo}
+        className="video-background"
+        type="video/mp4"
+      />
+      <div className="video-overlay">
+        <button className="btn-pause btn-hero" onClick={toggleVideo}>
+          {isPaused ? "SPELA UPP" : "PAUSA VIDEO"}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MainBanner;
